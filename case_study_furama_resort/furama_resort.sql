5efb9f940e3fbb25cfeb1d972bdde012686c536f
NguyenHuuQuoc <nguyenhuuwork@gmail.com>
2023-05-19 16:39:13 +0700

    bai tap

--- case_study_furama_resort/furama_resort.sql
+++ case_study_furama_resort/furama_resort.sql
@@ -299,12 +299,108 @@ join hop_dong_chi_tiet
 on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
 join dich_vu_di_kem
 on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
-where loai_khach.ten_loai_khach = 'Diamond' and (khach_hang.dia_chi like '%Vinh' or khach_hang.dia_chi like '%Quảng Ngãi') 
+where loai_khach.ten_loai_khach = 'Diamond' and (khach_hang.dia_chi like '%Vinh' or khach_hang.dia_chi like '%Quảng Ngãi'); 
 
 
  -- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, 
  -- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
  -- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021
+
+select hd.ma_hop_dong, nv.ho_ten as 'ho ten nhan vien', kh.ho_ten as 'ho ten khach hang', kh.so_dien_thoai, dv.ten_dich_vu, sum(hdct.so_luong)
+from hop_dong hd
+join nhan_vien nv 
+on hd.ma_nhan_vien = nv.ma_nhan_vien
+join khach_hang kh
+on hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu dv
+on dv.ma_dich_vu = hd.ma_dich_vu
+left join hop_dong_chi_tiet hdct
+on hdct.ma_hop_dong = hd.ma_hop_dong
+where quarter(ngay_lam_hop_dong) = '4' and year(ngay_lam_hop_dong) = '2020' and dv.ma_dich_vu not in (
+select dv.ma_dich_vu
+from dich_vu dv
+join hop_dong hd
+on dv.ma_dich_vu = hd.ma_dich_vu
+where ( quarter(ngay_lam_hop_dong)='1' or quarter(ngay_lam_hop_dong) = '2') and year(ngay_lam_hop_dong) = '2021')
+group by hd.ma_hop_dong;
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các 
+-- Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
+select dvdk.ten_dich_vu_di_kem, dvdk.ma_dich_vu_di_kem, sum(hdct.so_luong) as 'So lan duoc su dung'
+from hop_dong hd
+join hop_dong_chi_tiet hdct
+on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by  dvdk.ma_dich_vu_di_kem
+having sum(hdct.so_luong) = (
+select sum(hdct.so_luong)
+from hop_dong hd
+join hop_dong_chi_tiet hdct
+on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem
+order by sum(hdct.so_luong) desc
+limit 1);
+
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+
+select hd.ma_hop_dong, dvdk.ten_dich_vu_di_kem, ldv.ma_loai_dich_vu, ldv.ten_loai_dich_vu ,hdct.ma_hop_dong_chi_tiet, count(hdct.so_luong)
+from hop_dong hd
+join dich_vu dv
+on hd.ma_dich_vu = dv.ma_dich_vu
+join loai_dich_vu ldv
+on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+join hop_dong_chi_tiet hdct
+on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem 
+where dvdk.ma_dich_vu_di_kem in ( 
+select dvdk.ma_dich_vu_di_kem
+from hop_dong hd
+join hop_dong_chi_tiet hdct
+on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem
+having count(hdct.so_luong) =1
+)
+group by hd.ma_hop_dong, dvdk.ten_dich_vu_di_kem, ldv.ma_loai_dich_vu, ldv.ten_loai_dich_vu ,hdct.ma_hop_dong_chi_tiet;
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten,
+--  ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+
+select nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi
+from nhan_vien nv
+join trinh_do td
+on nv.ma_trinh_do = td.ma_trinh_do
+join bo_phan bp
+on bp.ma_bo_phan = nv.ma_bo_phan
+join hop_dong hd
+on nv.ma_nhan_vien = hd.ma_nhan_vien
+where year(hd.ngay_lam_hop_dong) between '2020' and '2021'
+group by nv.ma_nhan_vien
+having count(hd.ma_hop_dong) <=3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
